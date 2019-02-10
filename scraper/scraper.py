1ac5d9c4183ea4cf66175d644fc2a63358b4d21e
Derrick Li <derrickli@berkeley.edu>
2019-02-09 23:09:36 -0800

    updated scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,10 +1,12 @@
 from osisoft.pidevclub.piwebapi.pi_web_api_client import PIWebApiClient
 from osisoft.pidevclub.piwebapi.rest import ApiException
-from urllib3 import disable_warnings
+
+import urllib3
+import json
 
 path_prefix = "~/hack-davis-osi-bucket/"
 
-disable_warnings()
+urllib3.disable_warnings()
 
 client = PIWebApiClient("https://ucd-pi-iis.ou.ad3.ucdavis.edu/piwebapi", useKerberos=False, verifySsl=False)
 
@@ -14,7 +16,7 @@ attributes = ["Demand", "Cumulative Use"]
 
 query = "af:\\CEFS\\UCDAVIS\\Buildings\\{0}\\{1}|{2}"
 wifi_query = "af:\\REST POSTs to PI\\REST POSTs to PI\\UFL\\UFL\\Wifi Access Points\\ARC|Total_Count"
-fields = "Items.value;Items.timestamp;Items.unitabbr"
+# fields = "Items.value;Items.timestamp;Items.unitabbr"
 start = "*-24h"
 query_interval = "10m"
 
@@ -22,14 +24,30 @@ query_interval = "10m"
 for t in types:
     for a in attributes:
         try:
-            df = client.data.get_interpolated_values(query.format(buildings[0], t, a), start_time=start, interval=query_interval, selected_fields=fields)
-            # print(df)
-            df.to_json("../client/src/json/{0}_{1}_{2}.json".format(buildings[0], t, a))
-            print("Saving: {0} {1} {2}".format(buildings[0], t, a))
+            df = client.data.get_interpolated_values(query.format(buildings[0], t, a), start_time=start, interval=query_interval)
+            unit = df['UnitsAbbreviation'][0]
+            df = df[['Value', 'Timestamp']]
+            avg = df['Value'].mean()
+            data = df.to_dict()
+            data['Average'] = avg
+            data['Units'] = unit
+
+            with open("data/{0}_{1}_{2}.json".format(buildings[0], t, a), "w") as f:
+                json.dump(data, f)
+                print("Saving: {0} {1} {2}".format(buildings[0], t, a))
+
         except ApiException:
             print("No Endpoint for: {0} {1} {2}".format(buildings[0], t, a))
 
 
-wifi_df = client.data.get_interpolated_values(wifi_query, start_time=start, interval=query_interval, selected_fields=fields)
-wifi_df.to_json("../client/src/json/ARC_WiFi_TotalCount.json")
-print("Saving: ARC WiFi TotalCount")
+wifi_df = client.data.get_interpolated_values(wifi_query, start_time=start, interval=query_interval)
+unit = wifi_df['UnitsAbbreviation'][0]
+wifi_df = df[['Value', 'Timestamp']]
+avg = wifi_df['Value'].mean()
+data = wifi_df.to_dict()
+data['Average'] = avg
+data['Units'] = unit
+
+with open("data/ARC_WiFi_TotalCount.json", "w") as f:
+    json.dump(data, f)
+    print("Saving: ARC WiFi TotalCount")
